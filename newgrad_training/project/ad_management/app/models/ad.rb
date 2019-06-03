f207f0ec9e377191c8063c5e9d4516e37964baa9
ジャン・カイ <zhengk1994@jankainoMacBook-Pro.local>
2019-06-03 15:59:12 +0900

    deleted unnecessary files

--- newgrad_training/project/ad_management/app/models/ad.rb
+++ newgrad_training/project/ad_management/app/models/ad.rb
@@ -2,6 +2,6 @@ class Ad < ApplicationRecord
   mount_uploader :picture, PictureUploader
   validates :body, presence: true
   validates :price, presence: true
-  validates :url, presence: true
+  validates :url, presence: true, uniqueness: {case_sensitive: false}
 
 end
--- newgrad_training/w1/spec/factories/users.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :user do
-    sequence(:name) {|n| "User-#{n}"}
-    sequence(:email) {|n| "user-#{n}@example.com"}
-  end
-end
