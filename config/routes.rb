cd4152e8c515101e030c893781f5bd8c31c5c5c7
ジャン・カイ <zhengk1994@jankainoMacBook-Pro.local>
2019-06-05 19:31:05 +0900

    temporarily analysis json

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   resources :ads
   resources :users
+
+  namespace :api do
+    namespace :v1 do
+      resources :ads
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'ads#index'
   root 'users#index'
