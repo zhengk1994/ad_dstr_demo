d152f6b34576fc34022e21fbe5178e13f97c38f2
ジャン・カイ <zhengk1994@jankainoMacBook-Pro.local>
2019-06-05 18:52:44 +0900

    update README and form.html

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -1,7 +1,7 @@
 class Ad < ApplicationRecord
   mount_uploader :picture, PictureUploader
   validates :body, presence: true
-  validates :price, presence: true, numericality: { greater_than_or_equal_to: 20, less_than_or_equal_to: 30}
+  validates :price, presence: true, numericality: { greater_than: 0}
   validates :url, presence: true, uniqueness: {case_sensitive: false}
 
 end
