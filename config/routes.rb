45d70747fb060414a61eb24b80413f2371885f15
ジャン・カイ <zhengk1994@jankainoMacBook-Pro.local>
2019-06-18 16:42:43 +0900

    debug adsapi

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
+
+  get '/view' => 'ad_api#view'
+  get '/click' => 'ad_api#click'
+
   resources :ads
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'ads#index'
-  root 'users#index'
-
-
-
-
+  # root 'ads#index'
+  # root 'users#index'
+  #
+  #
+  #
+  #
   get 'users/index'
   get 'users/new'
   get 'users/edit'
