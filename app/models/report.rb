d5f64cbd5cf3ff34f71856ff7c98773e7f415634
ジャン・カイ <zhengk1994@jankainoMacBook-Pro.local>
2019-06-24 20:08:25 +0900

    report data storaged

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,2 +1,10 @@
 class Report < ApplicationRecord
+  validates :ad_id, presence: true
+  validates :adspot_id, presence: true
+  validates :imp, presence: true
+  validates :click, presence: true
+  validates :ctr, presence: true
+  validates :cpm, presence: true
+  validates :date, presence: true
+  validates :totalcost, presence: true
 end
